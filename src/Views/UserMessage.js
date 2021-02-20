cd0b6cd5e9ddca232ae356b5d44906a2db29e988
Ren Lin <renyuan7777@gmail.com>
2021-02-20 15:43:32 -0700

    Errors

---
+++ src/Views/UserMessage.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+export default class UserMessage extends Component
+{
+
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (
+            <p>
+                messages
+            </p>
+        )
+    }
+  export default UserMessage;
+}
