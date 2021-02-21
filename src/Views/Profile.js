cc1fb92606a46a434df29b63b9bbec6ef116a4d9
Carter Phillips <carter.joel@gmx.com>
2021-02-21 00:00:07 -0800

    user data is saved on the profile

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -7,9 +7,10 @@ export default class Profile extends Component
     constructor(props)
     {
         super(props);
-        this.state = {image: false}
+        this.state = {image: false, userData: false}
 
         GetUserData(this.props.uid, this.props.firebase).then(data => {
+            this.setState({userData: data})
             if(data.image){
                 this.props.firebase.storage().ref().child(this.props.uid + '.' + data.image).getDownloadURL().then(url => {
                     this.setState({image: url})
@@ -28,6 +29,7 @@ export default class Profile extends Component
                     profile
                 </p>
                 {this.state.image && (<img src={this.state.image}/>)}
+                {this.state.userData && JSON.stringify(this.state.userData)}
 
             </div>
         )
