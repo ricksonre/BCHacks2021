655738def3b1bcec38a149f7e9de4df3029f8beb
Carter Phillips <carter.joel@gmx.com>
2021-02-20 18:33:30 -0800

    ssss

--- src/GetMatches.js
+++ src/GetMatches.js
@@ -2,7 +2,7 @@ export default async function(uid, firebase){
 
 	const results = [];
 
-	await firebase.firestore().collection('users').doc('00000Example').collection('Matches').get().then(matchRefernces => {
+	await firebase.firestore().collection('users').doc(uid).collection('Matches').get().then(matchRefernces => {
 		matchRefernces.docs.forEach(doc => {
 			results.push(doc.data());
 			})
