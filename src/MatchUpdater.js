2ec3d637cf38b2b90776585de08aacbf68d05716
Carter Phillips <carter.joel@gmx.com>
2021-02-21 10:32:56 -0800

    matches page

--- src/MatchUpdater.js
+++ src/MatchUpdater.js
@@ -2,16 +2,23 @@
 
 export default function(firstUser, secondUser, firebase)
 {
-	const potentialMatches = firstUser.collection('PotentialMatches').doc(secondUser);
-	const potentialMatchesSecond = secondUser.collection('PotentialMatches').doc(firstUser);
+	const potentialMatches = firebase.firestore().collection('users').doc(firstUser).collection('PotentialMatches').doc(secondUser);
+	const potentialMatchesSecond = firebase.firestore().collection('users').doc(secondUser).collection('PotentialMatches').doc(firstUser);
 	return firebase.firestore().runTransaction(async(t) => {
 		potentialMatches.get().then(matchNum => {
-			matchNum = matchNum.data()['MatchCount']++;
-			t.set(potentialMatchesSecond, {MatchCount: matchNum})
-			t.set(potentialMatches, {MatchCount: matchNum})
-			if (matchNum > 3) {
-				t.set(firebase.firestore().collection('users').doc(firstUser).collection('Matches').doc(secondUser), {uid: secondUser})
-				t.set(firebase.firestore().collection('users').doc(secondUser).collection('Matches').doc(firstUser), {uid: firstUser})
+			console.log(matchNum.data())
+			if(matchNum.data()){
+			let num = matchNum.data().MatchCount+1;
+			console.log("MATCH  NUM", num)
+			potentialMatchesSecond.set({MatchCount: num})
+			potentialMatches.set({MatchCount: num})
+			if (num > 3) {
+				firebase.firestore().collection('users').doc(firstUser).collection('Matches').doc(secondUser).set( {uid: secondUser})
+				firebase.firestore().collection('users').doc(secondUser).collection('Matches').doc(firstUser).set({uid: firstUser})
+			}}
+			else{
+				potentialMatches.set({MatchCount: 1})
+				potentialMatchesSecond.set({MatchCount: 1})
 			}
 		})
 	})
