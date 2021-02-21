85adf74c7130a032cf74afca7e87c69b1d981494
Carter Phillips <carter.joel@gmx.com>
2021-02-20 21:39:51 -0800

    aa

--- src/RemoveMatch.js
+++ src/RemoveMatch.js
@@ -6,8 +6,8 @@ export default function(userOne,userTwo, firebase){
 		const potentialMatches = userOne.collection('PotentialMatches').doc(userTwo);
 		const potentialMatchesSecond = userTwo.collection('PotentialMatches').doc(userOne);
 		return db().runTransaction(async(t) => {
-			t.delete(potentialMatches).then(matchNum => {
-				t.delete(potentialMatchesSecond)
+			potentialMatches.delete().then(matchNum => {
+				potentialMatchesSecond.delete();
 				const userMatched = db.collection('users').doc(userOne).collection('Matches').doc(userTwo).get();
 				if(userMatched.exists){
 					t.delete( db.collection('users').doc(userOne).collection('Matches').doc(userTwo))
