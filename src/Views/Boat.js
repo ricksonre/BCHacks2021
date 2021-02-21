08fdc7d2165f8e97a3d89cd251f946d0c145cc41
ricksonre <ricksonre@gmail.com>
2021-02-20 16:07:38 -0800

    chat

--- src/Views/Boat.js
+++ src/Views/Boat.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import '../Styles/Boat.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Boat extends Component
 {
@@ -11,9 +13,103 @@ export default class Boat extends Component
     render()
     {
         return (
-            <p>
-                Boat
-            </p>
+            <div class="Boat">
+                <div class="UsersList">
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                    <div class="UserContainer">
+                        <i class="bi bi-card-image">
+                            <img class="img-thumbnail" />
+                        </i>
+                        <h3>
+                            User Name
+                        </h3>
+                        <hr/>
+                    </div>
+                </div>
+                <div class="ChatContainer">
+                    <div class="MessagesContainer">
+                        <div class="MessageContainer">
+                            <div class="Message User">
+                                message 1
+                            </div>
+                        </div>
+                        <div class="MessageContainer">
+                            <div class="Message Other">
+                                message 2
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ChatMessageInputContainer">
+                        <input type="text" placeholder="Message"></input>
+                        <button>Send</button>
+                    </div>
+                </div>
+                <div class="UserInformation">
+
+                </div>
+            </div>
         )
     }
 
