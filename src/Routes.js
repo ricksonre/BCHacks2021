2d22105e52d9a1c6f4720be27b5e10b1e4672d80
Carter Phillips <carter.joel@gmx.com>
2021-02-20 17:41:14 -0800

    working login

--- src/Routes.js
+++ src/Routes.js
@@ -4,23 +4,30 @@ import Ship from './Views/Ship'
 import Boat from './Views/Boat'
 import Profile from './Views/Profile'
 import AccountCreation from './Views/AccountCreation'
+import {Component} from 'react';
 
-export default function routes(){
-	return(
-		<div>
-		<BrowserRouter>
-			<main>
-				<div>
-					<Switch>
-						<Route path={'/ship'} component={() => <Ship firebase={this.props.firebase} uid={this.props.uid}/>}/>
-						<Route path={'/yourboat'} component={() => <Boat firebase={this.props.firebase} uid={this.props.uid}/>}/>
-						<Route path={'/profile'} component={() => <Profile firebase={this.props.firebase} uid={this.props.uid}/>}/>
-						<Route path={'/home'} component={() => <Home firebase={this.props.firebase} uid={this.props.uid}/>}/>
-						<Route path={'/accountcreation'} component={() => <AccountCreation firebase={this.props.firebase} uid={this.props.uid}/>}/>
-					</Switch>
-				</div>
-			</main>
-		</BrowserRouter>
-		</div>
-	)
+export default class routes extends Component{
+
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+			<div>
+				<BrowserRouter>
+					<main>
+						<div>
+							<Switch>
+								<Route path={'/ship'} component={() => <Ship firebase={this.props.firebase} uid={this.props.uid}/>}/>
+								<Route path={'/yourboat'} component={() =>(<Boat firebase={this.props.firebase} uid={this.props.uid}/>)}/>
+								<Route path={'/profile'} component={() => (<Profile firebase={this.props.firebase} uid={this.props.uid}/>)}/>
+								<Route path={'/home'} component={() => (<Home firebase={this.props.firebase} uid={this.props.uid}/>)}/>
+								<Route path={'/accountcreation'} component={() => (<AccountCreation firebase={this.props.firebase} uid={this.props.uid}/>)}/>
+							</Switch>
+						</div>
+					</main>
+				</BrowserRouter>
+			</div>
+		)
+	}
 }
--- src/Views/App.js
+++ src/Views/App.js
@@ -3,6 +3,9 @@ import Routes from '../Routes'
 import SideBar from './SideBar'
 import {Component} from 'react'
 import firebaseHelper from "../firebaseHelper";
+import $ from 'jquery';
+import googleImage from '../gsignn.png'
+import googleImage2 from '../gsignp.png'
 
 export default class app extends Component
 {
@@ -14,14 +17,44 @@ export default class app extends Component
 
         this.state={
             firebase: firebase,
-            uid: false,
+            uid: undefined,
             firebaseListeners: null
         }
     }
+    updateUserId = (userid) => {
+        console.log("HERE WITH UID OF", userid)
+        const ref = this.state.firebase.firestore().collection('users').doc(userid).collection('Messages')
+        ref.onSnapshot(colSnap => {
+            this.setState({firebaseListeners: colSnap})
+        })
+        this.setState({uid: userid.toString()})
+        console.log("HERE USER UPDATED")
+    }
 
-    updateUserId = (uid) => {
-        const listener = this.state.firebase.firestore().collection('users').doc(this.state.uid).collection('Messages')
-        this.setState({firebaseListeners: listener})
+    click_button(state)
+    {
+        if(state)
+            $("#signin").attr("src", googleImage2 );
+        else
+            $("#signin").attr("src", googleImage );
+
+    }
+
+    gSignIn()
+    {
+        var provider = new this.state.firebase.auth.GoogleAuthProvider();;
+        const that = this;
+        this.state.firebase.auth().signInWithPopup(provider)
+            .then(function(result)
+            {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                // The signed-in user info.
+                console.log("result", result.user.uid)
+                var user = result.user.uid;
+
+                that.updateUserId(user)
+
+                });
     }
 
     render(){
@@ -37,7 +70,13 @@ export default class app extends Component
                 </div>
            ) : (
                         <div className="App">
-                            Login here aaaa
+                            <button style={{margin: '0 0 0 0 ', padding: '0 0 0 0', border: "0", background: 'none', width: "35vh", height: "8vh",
+                                position: "fixed", left: "20.75%", top: "35%"}}
+                                    onClick={ ()=>{ this.gSignIn(); } }
+                                    onMouseDown={ ()=> { this.click_button(true); }}
+                                    onMouseUp={ ()=> { this.click_button(false); }}>
+                                <img id="signin" src={googleImage} style={{width:"100%", height:"100%"}}></img>
+                            </button>
                         </div>
                     )
 
