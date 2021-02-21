bc0e72895328757bb946724982e2391856138e52
Ren Lin <renyuan7777@gmail.com>
2021-02-20 20:41:57 -0700

    Color fix

--- src/Views/AccountCreation.js
+++ src/Views/AccountCreation.js
@@ -58,7 +58,8 @@ export default class AccountCreation extends Component{
 
 	render(){
 		return(
-      <div>
+      <div class = "tyra">
+
       <div class= "outer-container">
         <div class= "prompt">
           <p>Name</p>
@@ -66,30 +67,42 @@ export default class AccountCreation extends Component{
         </div>
       </div>
 
-      <div class= "prompt">
-        <p>Birthday</p>
-        <input type="date"/>
+      <div class= "outer-container">
+        <div class= "prompt">
+          <p>Birthday</p>
+          <input type="date"/>
+        </div>
       </div>
-      <div class= "prompt">
-        <p>Occupation</p>
-        <input type="text"/>
+
+      <div class= "outer-container">
+        <div class= "prompt">
+          <p>Occupation</p>
+          <input type="text"/>
+        </div>
       </div>
-      <div class= "prompt">
-        <p>Hobby</p>
-        <input type="text"/>
+
+      <div class= "outer-container">
+        <div class= "prompt">
+          <p>Hobby</p>
+          <input type="text"/>
+        </div>
       </div>
+
       <div class= "prompt">
         <p>Location</p>
         <input type="text"/>
       </div>
+
       <div class= "prompt">
         <p>Favorite Food</p>
         <input type="text"/>
       </div>
+
       <div class= "prompt">
         <p>Favorite Movie</p>
         <input type="text"/>
       </div>
+
       <div id= "ship" >
         <img id = "imageship" src="Boat.png" class="Icon"></img>
       </div>
